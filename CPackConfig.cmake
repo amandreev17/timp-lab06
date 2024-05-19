9fbbc13a7867a1578a02f77733cd0174281485d3
amandreev17 <144845041+amandreev17@users.noreply.github.com>
2024-05-19 10:56:52 +0300

    Update CPackConfig.cmake

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,17 +1,11 @@
 include(InstallRequiredSystemLibraries)
 
-set(CPACK_PACKAGE_CONTACT mihail_160505@@mail.ru)
+set(CPACK_PACKAGE_CONTACT a_m_andreev2005.ru)
 set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_PACKAGE_NAME "solver")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "static C++ library for solver")
-set(CPACK_PACKAGE_VENDOR "kuznetsovvvv")
+set(CPACK_PACKAGE_VENDOR "amandreev17")
 set(CPACK_PACKAGE_PACK_NAME "solver-${PRINT_VERSION}")
-
-set(CPACK_SOURCE_INSTALLED_DIRECTORIES 
-  "${CMAKE_SOURCE_DIR}/solver_application; solver_application"
-  "${CMAKE_SOURCE_DIR}/solver_lib; solver_lib"
-  "${CMAKE_SOURCE_DIR}/formatter_ex_lib; formatter_ex_lib"
-  "${CMAKE_SOURCE_DIR}/formatter_lib; formatter_lib")
 
 set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
 
@@ -24,8 +18,7 @@ set(CPACK_DEBIAN_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "solves equations")
 
-set(CPACK_GENERATOR "WIX")
+set(CPACK_GENERATOR "DEB;RPM")
 set(CPACK_RPM_PACKAGE_SUMMARY "solves equations")
-
 
 include(CPack)
